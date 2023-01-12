616517f4479d5a2d2b88d2bd51162517d4c52edc
chuangix <chuangix@outlook.com>
2023-01-12 23:01:49 +0800

    feat: add avy

--- lisp/init-tool.el
+++ lisp/init-tool.el
@@ -3,11 +3,8 @@
 (use-package which-key
   :ensure t
   :hook (after-init . which-key-mode)
-  :custom (which-key-idle-delay 0.5))
-
-(use-package fanyi
-  :ensure t
-  :commands fanyi-dwim fanyi-dwim2)
+  :config (which-key-setup-side-window-right)
+  :custom (which-key-idle-delay 1.5))
 
 (use-package session
   :ensure t
@@ -15,7 +12,28 @@
   :custom (desktop-save-mode t))
 
 (use-package helm
+  :ensure t
+  :bind
+  (("M-x"     . helm-M-x)
+   ("C-x C-f" . helm-find-files)))
+
+(use-package helm-descbinds
+  :after helm
   :ensure t)
+
+(use-package avy
+  :ensure t
+  :hook (after-init . avy-setup-default)
+  :bind
+  (("M-g c" . avy-goto-char-timer)
+   ("M-g l" . avy-goto-line))
+  :custom
+  (avy-background t)
+  (avy-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l ?q ?w ?e ?r ?u ?i ?o ?p)))
+
+(use-package fanyi
+  :ensure t
+  :commands fanyi-dwim fanyi-dwim2)
 
 (provide 'init-tool)
 
