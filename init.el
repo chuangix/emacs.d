ddf61214d5902930de33bad7dbd94cae587d70ce
chuangix <chuangix@outlook.com>
2023-01-16 22:37:42 +0800

    feat: setq custom-file custom.el

--- init.el
+++ init.el
@@ -9,6 +9,8 @@
   (add-to-list 'load-path (file-name-as-directory dir))
   (add-to-list 'load-path (file-name-as-directory (expand-file-name "lang" dir))))
 
+(setq custom-file (locate-user-emacs-file "custom.el"))
+
 ;; (require 'use-proxy)
 
 (require 'package)
