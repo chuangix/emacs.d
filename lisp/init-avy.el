ce202d8f36efefb72900f40f18417f7b92fbf120
chuangix <chuangix@outlook.com>
2023-01-28 23:13:15 +0800

    feat: hydra avy

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -9,13 +9,25 @@
 
 (use-package avy
   :ensure t
+  :after hydra
+  :config
+  (setq avy-background t
+        avy-all-windows t
+        avy-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l ?q ?w ?e ?r ?u ?i ?o ?p))
   :bind
-  (("M-g c" . avy-goto-char-timer)
-   ("M-g l" . avy-goto-line))
-  :custom
-  (avy-background t)
-  (avy-all-windows t)
-  (avy-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l ?q ?w ?e ?r ?u ?i ?o ?p)))
+  (("C-c a" . hydra-avy/body))
+  :hydra (hydra-avy
+          (:hint nil)
+          "
+^Character^         ^Word^           ^Line^            ^Region^
+--------------------------------------------------------------------------
+_c_ Goto Char    _w_ Goto Word    _l_ Goto Line    _r_ Copy Region
+"
+          ("c" avy-goto-char-timer :exit t)
+          ("w" avy-goto-word-0 :exit t)
+          ("l" avy-goto-line :exit t)
+          ("r" avy-copy-region :exit t)
+          ("q" nil)))
 
 (use-package avy-zap
   :after avy
