f5f40bcf0bbb9c422dba094692cb023adf318088
chuangix <chuangix@outlook.com>
2023-02-03 22:52:18 +0800

    fix: exterminate warning

--- lisp/check-proxy.el
+++ lisp/check-proxy.el
@@ -7,25 +7,27 @@
 
 ;;; Code:
 
-(when (file-exists-p (locate-user-emacs-file "lisp/user-preference.el"))
-  (require 'user-preference)
-  (let ((http (alist-get 'https-proxy *user-preference*))
-         (https (alist-get 'https-proxy *user-preference*)))
-    (or (eq nil http)
-        (eq nil https)
-        (progn
-          (setq url-proxy-services ())
-          (push (cons "http" http) url-proxy-services)
-          (push (cons "https" https) url-proxy-services)
-          (push (cons "no_proxy" "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)") url-proxy-services))))
-  (let ((account (alist-get 'proxy-account *user-preference*))
-        (password (alist-get 'proxy-password *user-preference*)))
-    (or (eq nil account)
-        (eq nil password)
-        (setq url-http-proxy-basic-auth-storage
-            (list (list "proxy.neusoft.com:8080"
-                        (cons "Input your LDAP UID !"
-                              (base64-encode-string (concat account ":" password)))))))))
+(eval-and-compile
+  (when (require 'user-preference nil t)
+    (let ((http (alist-get 'https-proxy *user-preference*))
+          (https (alist-get 'https-proxy *user-preference*)))
+      (or (eq nil http)
+          (eq nil https)
+          (progn
+            (setq url-proxy-services ())
+            (push (cons "http" http) url-proxy-services)
+            (push (cons "https" https) url-proxy-services)
+            (push (cons "no_proxy" "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)") url-proxy-services))))
+    (let ((account (alist-get 'proxy-account *user-preference*))
+          (password (alist-get 'proxy-password *user-preference*)))
+      (or (eq nil account)
+          (eq nil password)
+          (defconst url-http-proxy-basic-auth-storage
+            (list
+             (list "proxy.neusoft.com:8080"
+                   (cons "Input your LDAP UID !"
+                         (base64-encode-string
+                          (concat account ":" password))))))))))
 
 (provide 'check-proxy)
 
--- lisp/lang/init-lang-cpp.el
+++ lisp/lang/init-lang-cpp.el
@@ -1,33 +1,36 @@
 ;;; init-lang-cpp.el --- CPP Language Configuration -*- lexical-binding: t -*-
+
 ;;; Commentary:
+
 ;;; Code:
 
 (use-package cc-mode
   :ensure nil
   :mode ("\\.cc\\'\\|\\.cxx\\'\\|\\.cpp\\'\\|\\.hxx\\'\\|\\.hpp\\'" . c++-mode))
 
-(setq flycheck-clang-language-standard "c++11")
+(defconst flycheck-clang-language-standard "c++11")
 
-(when (file-exists-p (locate-user-emacs-file "lisp/user-preference.el"))
-  (require 'user-preference)
-  (let ((clangd (alist-get 'clangd-executable *user-preference*))
-        (lldb (alist-get 'lldb-executable *user-preference*)))
-    (unless (eq nil clangd)
-      (with-eval-after-load 'lsp-mode
-        (setq lsp-clients-clangd-executable clangd
-              lsp-clients-clangd-args '("-j=2"
-                                        "--background-index"
-                                        "--clang-tidy"
-                                        "--completion-style=bundled"
-                                        "--pch-storage=memory"
-                                        "--header-insertion=iwyu"
-                                        "--header-insertion-decorators"))))
-    (unless (eq nil lldb)
-      (use-package dap-lldb
-        :config
-        (setq dap-lldb-debug-program '(lldb))
-        (setq dap-lldb-debugged-program-function
-	      (lambda () (read-file-name "Select file to debug: ")))))))
+(eval-and-compile
+  (and (require 'user-preference nil t)
+       (let ((clangd (alist-get 'clangd-executable *user-preference*))
+             (lldb (alist-get 'lldb-executable *user-preference*)))
+         (unless (eq nil clangd)
+           (with-eval-after-load 'lsp-mode
+             (setq lsp-clients-clangd-executable clangd)
+             (setq lsp-clients-clangd-args
+                   '("-j=2"
+                     "--background-index"
+                     "--clang-tidy"
+                     "--completion-style=bundled"
+                     "--pch-storage=memory"
+                     "--header-insertion=iwyu"
+                     "--header-insertion-decorators"))))
+         (unless (eq nil lldb)
+           (use-package dap-lldb
+             :config
+             (setq dap-lldb-debug-program '(lldb))
+             (setq dap-lldb-debugged-program-function
+	           (lambda () (read-file-name "Select file to debug: "))))))))
 
 (use-package cmake-mode
   :mode (("CMakeLists\\.txt\\'" . cmake-mode)
