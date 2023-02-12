ad8bf7ff412e37bee3df5fc306e679fb0890d61d
chuangix <chuangix@outlook.com>
2023-02-12 21:19:59 +0800

    feat: add some key-bindings of cursor movement

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -14,15 +14,20 @@
   '("Cursor Movement"
     (("c" avy-goto-char-timer "jump to char" :exit t)
      ("w" avy-goto-word-or-subword-1 "jump to word" :exit t)
-     ("l" avy-goto-line "jump to line" :exit t))))
+     ("l" avy-goto-line "jump to line" :exit t)
+     ("L" avy-copy-line "copy and insert line" :exit t)
+     ("R" avy-copy-region "copy and insert region" :exit t)
+     ("D" avy-zap-to-char "delete to char" :exit t))))
 
 (eval
- `(major-mode-hydra-define emacs-lisp-mode (:title "Elisp" :color amaranth :quit-key "q")
-    ,(append *hydra-cursor-movement* nil)))
+ `(major-mode-hydra-define emacs-lisp-mode (:color amaranth :quit-key "q")
+    ,(append *hydra-cursor-movement*
+             nil)))
 
 (eval
- `(major-mode-hydra-define c++-mode (:title "C++" :color amaranth :quit-key "q")
-    ,(append *hydra-cursor-movement* nil)))
+ `(major-mode-hydra-define c++-mode (:color amaranth :quit-key "q")
+    ,(append *hydra-cursor-movement*
+             nil)))
 
 (provide 'init-hydra)
 
